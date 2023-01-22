81264c4c787a8e19366eaafdd10f5ce104cf294f
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-22 20:16:36 +0100

    add pagination

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
 from app import models
 from app.database import engine
 from app.endpoints.users import users_router
 
-
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-
+add_pagination(app)
 app.include_router(users_router)

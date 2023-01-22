b25e3c6fdb7cc8506f4664a534ee6bbaadeb9eed
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-23 00:36:33 +0100

    fix auth

--- app/endpoints/auth.py
+++ app/endpoints/auth.py
@@ -38,8 +38,8 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 async def get_current_user(
-    db: Session,
     token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db),
 ) -> schemas.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -48,7 +48,7 @@ async def get_current_user(
     )
     try:
         payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
-        username: str = payload.get("username")
+        username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = schemas.TokenData(username=username)
@@ -58,6 +58,12 @@ async def get_current_user(
     if user is None:
         raise credentials_exception
     return user
+
+
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)) -> schemas.User:
+    if current_user.is_active is False:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 
 @auth_router.post("/token", response_model=schemas.Token)
@@ -70,10 +76,10 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"username": user.username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
 @auth_router.get("/users/me/", response_model=schemas.User)
-async def read_users_me(db: Session = Depends(get_db)):
-    return await get_current_user(db)
+async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
+    return current_user

14bf94e1dde8516088ee6b5be42f3506130d866c
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-24 00:33:02 +0100

    add getting budget by id

--- app/crud.py
+++ app/crud.py
@@ -35,6 +35,10 @@ def get_budgets(db: Session) -> Union[list[models.Budget], None]:
     return db.query(models.Budget).all()
 
 
+def get_budget_by_id(db: Session, budget_id: int) -> Union[models.Budget, None]:
+    return db.query(models.Budget).filter(models.Budget.id == budget_id).first()
+
+
 def get_budgets_by_owner_id(db: Session, owner_id: int) -> Union[list[models.Budget], None]:
     return db.query(models.Budget).filter(models.Budget.owner_id == owner_id).all()
 
@@ -42,6 +46,16 @@ def get_budgets_by_owner_id(db: Session, owner_id: int) -> Union[list[models.Bud
 def create_budget(db: Session, budget: schemas.BudgetCreate, owner_id: int) -> models.Budget:
     db_budget = models.Budget(**budget.dict(), owner_id=owner_id)
     db.add(db_budget)
+    db.commit()
+    db.refresh(db_budget)
+    return db_budget
+
+
+def add_users_to_budget(db: Session, budget_id: int) -> models.Budget:
+    db_budget = get_budget_by_id(db, budget_id)
+
+    # do something
+
     db.commit()
     db.refresh(db_budget)
     return db_budget
